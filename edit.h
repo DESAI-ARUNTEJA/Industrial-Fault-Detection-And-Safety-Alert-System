39ce164f482246c141ad35dd95aee14fb4335a87
DESAI-ARUNTEJA <aruntejadesai29@gmail.com>
2026-09-10 20:49:21 +0530

    Add files via upload

---
+++ edit.h
@@ -0,0 +1,16 @@
+#include "types.h"
+void myitoa(u32 ,u8 *); 
+void edit_password(void);
+void edit_menu(void);
+void edit_setpoint(void);
+void set_temperature(void);
+void set_humidity(void);
+void extract(char *p);
+void edit_rtc(void);
+void edit_hour(void);
+void edit_minute(void);
+void edit_second(void);
+void edit_day(void);
+void edit_date(void);
+void edit_month(void);
+void edit_year(void);
