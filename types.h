f24fd461c0778cc66b273d0c25b9bd7dfd64a2bf
DESAI-ARUNTEJA <aruntejadesai29@gmail.com>
2026-09-10 21:02:43 +0530

    Add files via upload

---
+++ types.h
@@ -0,0 +1,12 @@
+#ifndef __TYPES_H__
+#define _TYPES_H__
+typedef unsigned char u8;
+typedef const unsigned char cu8;
+typedef char s8;
+typedef unsigned  short u16;
+typedef char s16;
+typedef unsigned int u32;
+typedef int s32;
+typedef float f32;
+typedef double f64;
+#endif
